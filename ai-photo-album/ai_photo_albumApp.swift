639c4bc996eb1ef76c5f50bc3c0ba4f9c408bf7f
Krishiv Manyam <krishivmanyam@gmail.com>
2026-01-09 17:47:25 -0800

    Initial Commit

---
+++ ai-photo-album/ai_photo_albumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ai_photo_albumApp.swift
+//  ai-photo-album
+//
+//  Created by Krishiv Manyam on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct ai_photo_albumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
